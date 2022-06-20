dea0a0d9106374afdc2aebc3f128348bc47dfaa2
pankaj sharma <pankajdxba@hotmail.com>
2022-06-20 18:21:58 +0530

    UI Development 20th Jun 1st commit

---
+++ src/app/pages/listproperties/propertyinfo/propertyinfo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { SecondHeaderComponent } from '../../../second-header/second-header.component';
+import {FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-propertyinfo',
+  templateUrl: './propertyinfo.component.html',
+  styleUrls: ['./propertyinfo.component.scss']
+})
+export class PropertyinfoComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+  }
+
+}
