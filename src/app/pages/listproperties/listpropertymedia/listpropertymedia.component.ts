3fc9e5325b7ca8c35e87e1fcfc9537788ce0b80d
pankaj sharma <pankajdxba@hotmail.com>
2022-06-24 10:02:05 +0530

    Ui Development 24th Jun 2022

--- src/app/pages/listproperties/listpropertymedia/listpropertymedia.component.ts
+++ src/app/pages/listproperties/listpropertymedia/listpropertymedia.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { SecondHeaderComponent } from '../../../second-header/second-header.component';
+import {FormControl, Validators} from '@angular/forms';
+import * as $ from 'jquery';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FileUploadService } from '../../../service/file-upload.service';
+
 
 @Component({
   selector: 'app-listpropertymedia',
@@ -6,10 +13,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listpropertymedia.component.scss']
 })
 export class ListpropertymediaComponent implements OnInit {
+  selectedFiles?: FileList;
+  progressInfos: any[] = [];
+  message: string[] = [];
+  previews: string[] = [];
+  imageInfos?: Observable<any>;
+  constructor(private uploadService: FileUploadService) { }
+  ngOnInit() {
+    $(document).ready(function(){
+      $('.dropdown-toggle').click(function(){
+      $(this).next().toggleClass('active');
+      });
+  });
+  this.imageInfos = this.uploadService.getFiles();
+  }
+  selectFiles(event: any): void {
+    this.message = [];
+    this.progressInfos = [];
+    this.selectedFiles = event.target.files;
 
-  constructor() { }
+    this.previews = [];
+    if (this.selectedFiles && this.selectedFiles[0]) {
+      const numberOfFiles = this.selectedFiles.length;
+      for (let i = 0; i < numberOfFiles; i++) {
+        const reader = new FileReader();
 
-  ngOnInit(): void {
+        reader.onload = (e: any) => {
+          this.previews.push(e.target.result);
+        };
+
+        reader.readAsDataURL(this.selectedFiles[i]);
+      }
+    }
   }
 
+  upload(idx: number, file: File): void {
+    this.progressInfos[idx] = { value: 0, fileName: file.name };
+
+    if (file) {
+      this.uploadService.upload(file).subscribe(
+        (event: any) => {
+          if (event.type === HttpEventType.UploadProgress) {
+            this.progressInfos[idx].value = Math.round(
+              (100 * event.loaded) / event.total
+            );
+          } else if (event instanceof HttpResponse) {
+            const msg = 'Uploaded the file successfully: ' + file.name;
+            this.message.push(msg);
+            this.imageInfos = this.uploadService.getFiles();
+          }
+        },
+        (err: any) => {
+          this.progressInfos[idx].value = 0;
+          const msg = 'Could not upload the file: ' + file.name;
+          this.message.push(msg);
+        }
+      );
+    }
+  }
+
+  uploadFiles(): void {
+    this.message = [];
+
+    if (this.selectedFiles) {
+      for (let i = 0; i < this.selectedFiles.length; i++) {
+        this.upload(i, this.selectedFiles[i]);
+      }
+    }
+  }
 }
