8a398e7e4dbe6838029386cc5734951aa9ac2401
aamiranwar111 <id.aamiranwar@gmail.com>
2022-06-27 15:31:56 +0500

    changes

--- src/app/service/app.service.ts
+++ src/app/service/app.service.ts
@@ -7,15 +7,30 @@ import { Injectable } from '@angular/core';
 export class AppService {
   apiurl='https://www.ovaluate.com/api/';
   id=0;
-
+  headers: HttpHeaders = new HttpHeaders ({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + localStorage.getItem('token')
+});
   constructor(private http:HttpClient) {
     
   }
+  LoadPropertyCategories(){
+    
+    return this.http.get(this.apiurl+'PropertyCategories',{headers:this.headers});
+  }
+  LoadPropertyTypes(){
+    return this.http.get(this.apiurl+'PropertyType/');
+  }
+
   LoadBlogs(){
+    // BlogBanners
     return this.http.get(this.apiurl+'blogs');
   }
   LoadBlogById(){
     return this.http.get(this.apiurl+'blog/'+this.id);
   }
+
+  
+  
   
 }
