8d1eb3fe267cb5ec39cdb927b2190df56694fc29
pankaj sharma <pankajdxba@hotmail.com>
2022-06-29 22:42:30 +0530

    UI Development Property valuation

--- src/app/pages/PropertyValuation/property-types/property-types.component.ts
+++ src/app/pages/PropertyValuation/property-types/property-types.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SecondHeaderComponent } from '../../../second-header/second-header.component';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-property-types',
@@ -6,7 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-types.component.scss']
 })
 export class PropertyTypesComponent implements OnInit {
-
+  plus= '../../../../assets/images/plus.svg'
+  country = [
+    {viewValue: 'India',value: 'india'},
+    {viewValue: 'UAE',value: 'UAE'},
+    {viewValue: 'America',value: 'America'},
+  ];
+  city = [
+    {viewValue: 'India',value: 'india'},
+    {viewValue: 'UAE',value: 'UAE'},
+    {viewValue: 'America',value: 'America'},
+  ];
+  bedrooms = [
+    {viewValue: '01',value: 'bedroom'},
+    {viewValue: '02',value: 'bedroom'},
+    {viewValue: '03',value: 'bedroom'},
+  ];
+  bathroom = [
+    {viewValue: '01',value: 'bedroom'},
+    {viewValue: '02',value: 'bedroom'},
+    {viewValue: '03',value: 'bedroom'},
+  ];
   constructor() { }
 
   ngOnInit(): void {
