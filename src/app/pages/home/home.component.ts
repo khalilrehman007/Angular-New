5d110baa5c4555f25e8583ee62e301cf61c2842b
aamiranwar111 <id.aamiranwar@gmail.com>
2022-06-30 15:51:52 +0500

    changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,6 +8,9 @@ import {NgbNav} from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from '../../header/header.component';
 import { FooterComponent } from '../../footer/footer.component';
 import { AppService } from 'src/app/service/app.service';
+import { FormControl, FormGroup } from '@angular/forms';
+import { NotificationService } from 'src/app/service/notification.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -22,6 +25,8 @@ export class HomeComponent implements OnInit {
   brandimg = 'assets/images/better-home.svg'
   listingsliderimg = 'assets/images/property-listing-slider-img.png'
   blogs: any;
+  submitted = false;
+  responsedata: any;
   dynamicSlides = [
     {
       id: 'slide1',
@@ -226,8 +231,12 @@ export class HomeComponent implements OnInit {
   }
   categories: any;
   types: any;
-  
-  constructor(private service:AppService) {
+  search = new FormGroup({
+    Name: new FormControl(""),
+    Type: new FormControl(""),
+    RentalType: new FormControl("")
+  })
+  constructor(private service:AppService,private route:Router,private notifyService : NotificationService) {
     this.LoadPropertyCategories() 
     this.LoadBlogs();
   }
@@ -239,7 +248,27 @@ export class HomeComponent implements OnInit {
         });
     });
   }
-
+  ProceedSearch() {
+    this.submitted = true;
+    if (this.search.invalid) {
+      return;
+    }
+    if (this.search.valid) {
+      console.log(this.search.value)
+      this.service.ProceedSearch(this.search.value).subscribe(result => {
+        if(result!=null ){
+          this.responsedata=result;
+          this.responsedata.data =this.responsedata.data;
+          localStorage.setItem('token',this.responsedata.data.refreshToken)
+          localStorage.setItem('user',JSON.stringify(this.responsedata.data))
+          this.notifyService.showSuccess(this.responsedata.message, "");
+        }else{
+          this.notifyService.showError("Unable to login", ""); 
+        }
+        this.route.navigate([''])
+      });
+    }
+  }
   LoadPropertyCategories(){
     this.service.LoadPropertyCategories().subscribe(data=>{
       this.categories=data;
