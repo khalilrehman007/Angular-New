53a37d2fd09cc5858c7a2348bcb5e2ef3272f5ed
pankaj sharma <pankajdxba@hotmail.com>
2022-06-30 17:48:10 +0530

    UI Development Changes for valuation pages

--- src/app/pages/PropertyValuation/property-documents/property-documents.component.ts
+++ src/app/pages/PropertyValuation/property-documents/property-documents.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SecondHeaderComponent } from '../../../second-header/second-header.component';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-property-documents',
@@ -6,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-documents.component.scss']
 })
 export class PropertyDocumentsComponent implements OnInit {
-
+  upload ='../../../../assets/images/icons/upload-1.svg'
+  trash ='../../../../assets/images/icons/Trash.svg'
+  edit ='../../../../assets/images/icons/edit.svg'
+  checkmark ='../../../../assets/images/icons/checkmark-circle.svg'
+  file: string;
+  affecton: string;
+  propertys: string;
+  emirate: string;
+  handleChange(files: FileList) {
+    if (files && files.length) {
+      this.file = files[0].name;
+    }
+  }
+  affection(files: FileList) {
+    if (files && files.length) {
+      this.affecton = files[0].name;
+    }
+  }
+  property(files: FileList) {
+    if (files && files.length) {
+      this.propertys = files[0].name;
+    }
+  }
+  emiratesfun(files: FileList) {
+    if (files && files.length) {
+      this.emirate = files[0].name;
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
