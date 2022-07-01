b3da8f64d4d3d3d3dbf665c4238a9dda4ca115c8
Faizan Ahmad <faizandeveloper09@gmail.com>
2022-07-01 13:15:49 +0500

    Homepage rent and buy tabs fixed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -27,38 +27,164 @@ export class HomeComponent implements OnInit {
   blogs: any;
   submitted = false;
   responsedata: any;
-  dynamicSlides = [
-    {
-      id: 'slide1',
-      src:'assets/images/property/1.png',
-      alt:'Side 1',
-      title:'Side 1'
-    },
-    {
-      id: 'slide2',
-      src:'assets/images/property/1.png',
-      alt:'Side 2',
-      title:'Side 2'
-    },
-    {
-      id: 'slide3',
-      src:'assets/images/property/1.png',
-      alt:'Side 3',
-      title:'Side 3'
-    },
-    {
-      id: 'slide4',
-      src:'assets/images/property/1.png',
-      alt:'Side 4',
-      title:'Side 4'
-    },
-    {
-      id: 'slide5',
-      src:'assets/images/property/1.png',
-      alt:'Side 5',
-      title:'Side 5'
-    }
-  ]
+  dynamicSlides1:any = [];
+  dynamicSlides2:any = [];
+  oldData1() {
+    this.dynamicSlides1 = [
+      {
+        id: 'slide1',
+        src:'assets/images/property/1.png',
+        alt:'Side 1',
+        title:'Side 1',
+        price:"250,000AED"
+      },
+      {
+        id: 'slide2',
+        src:'assets/images/property/1.png',
+        alt:'Side 2',
+        title:'Side 2',
+        price:"250,000AED"
+      },
+      {
+        id: 'slide3',
+        src:'assets/images/property/1.png',
+        alt:'Side 3',
+        title:'Side 3',
+        price:"250,000AED"
+      },
+      {
+        id: 'slide4',
+        src:'assets/images/property/1.png',
+        alt:'Side 4',
+        title:'Side 4',
+        price:"250,000AED"
+      },
+      {
+        id: 'slide5',
+        src:'assets/images/property/1.png',
+        alt:'Side 5',
+        title:'Side 5',
+        price:"250,000AED"
+      }
+    ]
+  }
+  newData1() {
+    this.dynamicSlides1 = [
+      {
+        id: 'slide1',
+        src:'assets/images/property/1.png',
+        alt:'Side 1',
+        title:'Side 56',
+        price:"350,000AED"
+      },
+      {
+        id: 'slide2',
+        src:'assets/images/property/1.png',
+        alt:'Side 2',
+        title:'Side 2',
+        price:"350,000AED"
+      },
+      {
+        id: 'slide3',
+        src:'assets/images/property/1.png',
+        alt:'Side 3',
+        title:'Side 3',
+        price:"350,000AED"
+      },
+      {
+        id: 'slide4',
+        src:'assets/images/property/1.png',
+        alt:'Side 4',
+        title:'Side 4',
+        price:"350,000AED"
+      },
+      {
+        id: 'slide5',
+        src:'assets/images/property/1.png',
+        alt:'Side 5',
+        title:'Side 5',
+        price:"350,000AED"
+      }
+    ]
+  }
+  oldData2() {
+    this.dynamicSlides2 = [
+      {
+        id: 'slide1',
+        src:'assets/images/property/1.png',
+        alt:'Side 1',
+        title:'Side 1',
+        price:"250,000AED"
+      },
+      {
+        id: 'slide2',
+        src:'assets/images/property/1.png',
+        alt:'Side 2',
+        title:'Side 2',
+        price:"250,000AED"
+      },
+      {
+        id: 'slide3',
+        src:'assets/images/property/1.png',
+        alt:'Side 3',
+        title:'Side 3',
+        price:"250,000AED"
+      },
+      {
+        id: 'slide4',
+        src:'assets/images/property/1.png',
+        alt:'Side 4',
+        title:'Side 4',
+        price:"250,000AED"
+      },
+      {
+        id: 'slide5',
+        src:'assets/images/property/1.png',
+        alt:'Side 5',
+        title:'Side 5',
+        price:"250,000AED"
+      }
+    ]
+  }
+  newData2() {
+    this.dynamicSlides2 = [
+      {
+        id: 'slide1',
+        src:'assets/images/property/1.png',
+        alt:'Side 1',
+        title:'Side 56',
+        price:"350,000AED"
+      },
+      {
+        id: 'slide2',
+        src:'assets/images/property/1.png',
+        alt:'Side 2',
+        title:'Side 2',
+        price:"350,000AED"
+      },
+      {
+        id: 'slide3',
+        src:'assets/images/property/1.png',
+        alt:'Side 3',
+        title:'Side 3',
+        price:"350,000AED"
+      },
+      {
+        id: 'slide4',
+        src:'assets/images/property/1.png',
+        alt:'Side 4',
+        title:'Side 4',
+        price:"350,000AED"
+      },
+      {
+        id: 'slide5',
+        src:'assets/images/property/1.png',
+        alt:'Side 5',
+        title:'Side 5',
+        price:"350,000AED"
+      }
+    ]
+  }
   tenantsslide = [
     {
       id: 'slide1',
@@ -239,6 +365,8 @@ export class HomeComponent implements OnInit {
   constructor(private service:AppService,private route:Router,private notifyService : NotificationService) {
     this.LoadPropertyCategories() 
     this.LoadBlogs();
+    this.oldData1();
+    this.oldData2();
   }
   ngOnInit():void {
     
