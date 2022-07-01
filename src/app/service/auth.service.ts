2f18097eae1784672e9fe85a3e2f4f98d4efdffa
hikmatullah <hikmatullah@eplanetcom.com>
2022-07-01 14:57:51 +0500

    changes in auth service file and revert home Rent component

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
 
 
   public isAuthenticated(): boolean {
-    const token = localStorage.getItem('token');
+    // const token = localStorage.getItem('token');
     // Check whether the token is expired and return
     // true or false
     // return !this.jwtHelper.isTokenExpired(token);
