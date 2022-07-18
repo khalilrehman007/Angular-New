05cd13b067b4840dd28f376e232614fa4a8a94f1
aslamdxb <aslamdxba@hotmail.com>
2022-07-18 13:50:42 +0500

    Residential and commercial API Integrations

--- src/app/service/app.service.ts
+++ src/app/service/app.service.ts
@@ -62,4 +62,13 @@ export class AppService {
   FittingTypes() {
     return this.http.get(this.apiurl + 'FittingTypes/');
   }
+  PropertyListingTypes() {
+    return this.http.get(this.apiurl + 'PropertyListingTypes/');
+  }
+  LatestPropertiesListingResidential(id: number) {
+    return this.http.get(this.apiurl + 'LatestPropertiesListingResidential/' + id);
+  }
+  LatestPropertiesListingCommercial(id: number) {
+    return this.http.get(this.apiurl + 'LatestPropertiesListingCommercial/' + id);
+  }
 }
