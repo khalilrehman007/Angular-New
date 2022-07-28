de7d7b7e395cea3dee439eb59b419b1ec1d6c91c
luqman.webs <luqmandxb@hotmail.com>
2022-07-28 10:47:01 +0500

    cms about us wip

--- src/app/pages/privacy-policy/privacy-policy.component.ts
+++ src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -8,8 +8,18 @@ import { AppService } from 'src/app/service/app.service';
 })
 export class PrivacyPolicyComponent implements OnInit {
 
+  image:any;
+  text:any;
+  title:any;
+  subHeading:any;
   constructor(private api: AppService) {
     this.api.PrivacyPolicy().subscribe((result:any)=> {
+      this.text = result.data.pageCaptionText.substr(0,1).toUpperCase()+result.data.pageCaptionText.substr(1);
+      this.title = this.text.split("</br>")[0];
+      this.subHeading = this.text.split("</br>")[1];
+      this.image = "https://beta.ovaluate.com/" + result.data.fileUrl;
+      this.image = this.image.replaceAll("\\", "/");
+      $(".inner-page-banner-sec").css({"background-image":"url('"+this.image+"')"})
       $(".cms_content-paragraph").append(result.data.pageContent);
     })
   }
--- src/app/pages/term-conditions/term-conditions.component.ts
+++ src/app/pages/term-conditions/term-conditions.component.ts
@@ -9,8 +9,14 @@ import { AppService } from 'src/app/service/app.service';
 export class TermConditionsComponent implements OnInit {
 
   image:any;
+  text:any;
+  title:any;
+  subHeading:any;
   constructor(private api: AppService) {
     this.api.TermsCondition().subscribe((result:any)=> {
+      this.text = result.data.pageCaptionText.substr(0,1).toUpperCase()+result.data.pageCaptionText.substr(1);
+      this.title = this.text.split("</br>")[0];
+      this.subHeading = this.text.split("</br>")[1];
       this.image = "https://beta.ovaluate.com/" + result.data.fileUrl;
       this.image = this.image.replaceAll("\\", "/");
       $(".inner-page-banner-sec").css({"background-image":"url('"+this.image+"')"})
