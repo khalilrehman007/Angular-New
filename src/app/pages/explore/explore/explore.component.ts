afa11817834733df9e87558bc6a5aaab9aa1c8fa
pankaj sharma <pankajdxba@hotmail.com>
2022-08-11 13:42:07 +0530

    Explore Search Design

--- src/app/pages/explore/explore/explore.component.ts
+++ src/app/pages/explore/explore/explore.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ElementRef, ViewChild } from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
 import { AppService } from 'src/app/service/app.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
+import {MatChipInputEvent} from '@angular/material/chips';
+import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-explore',
@@ -78,9 +83,53 @@ export class ExploreComponent implements OnInit {
         }
       }
     });
+    this.searchfilterExplore = this.searchctrlExplore.valueChanges.pipe(
+      startWith(null),
+      map((fruit: string | null) => (fruit ? this._filter(fruit) : this.searchListExplore.slice())),
+    );
   }
 
   ngOnInit(): void {
   }
+  // Search Code
+  separatorKeysCodesExplore: number[] = [ENTER, COMMA];
+  searchctrlExplore = new FormControl('');
+  searchfilterExplore: Observable<string[]>;
+  SearchKeywordExplore: string[] = [];
+  searchListExplore: string[] = ['Dubai', 'UAE', 'Dubai', 'UAE', 'Dubai'];
 
+  @ViewChild('SearchInput') SearchInput: ElementRef<HTMLInputElement>;
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.SearchKeywordExplore.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+
+    this.searchctrlExplore.setValue(null);
+  }
+
+  remove(fruit: string): void {
+    const index = this.SearchKeywordExplore.indexOf(fruit);
+
+    if (index >= 0) {
+      this.SearchKeywordExplore.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.SearchKeywordExplore.push(event.option.viewValue);
+    this.SearchInput.nativeElement.value = '';
+    this.searchctrlExplore.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.searchListExplore.filter(fruit => fruit.toLowerCase().includes(filterValue));
+  }
 }
