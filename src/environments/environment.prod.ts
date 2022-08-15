adb0ed32989ee33dcab3514df5ae1779eea146ca
luqman.webs <luqmandxb@hotmail.com>
2022-08-15 19:07:43 +0500

    Changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1Ijoib21yYW4teW91MjIiLCJhIjoiY2w2dW13d3JuMDY5bjNqcWcydm5weWVzdyJ9.2eYmMQJurCwFANwbEyIFHQ'
+  }
 };
