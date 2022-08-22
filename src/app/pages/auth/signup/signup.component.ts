5b0313ba9733f6fb0a50ddd6aa79c8d049911fba
roshaan9035 <shanijutt9035@gmail.com>
2022-08-22 15:44:34 +0500

    Agent section property detail done

--- src/app/pages/auth/signup/signup.component.ts
+++ src/app/pages/auth/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SecondHeaderComponent } from '../../../second-header/second-header.component';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import * as $ from 'jquery';
 import { AuthService } from 'src/app/service/auth.service';
 import { Router } from '@angular/router';
@@ -22,9 +22,9 @@ export class SignupComponent implements OnInit {
   signUpPropertytype: any = [];
 
   // emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  constructor(private service: AuthService, private route: Router, private notifyService: NotificationService, private response: AppService) {
+  constructor(private service: AuthService,private route:Router,private notifyService : NotificationService, private response:AppService) {
     localStorage.clear();
-    this.response.ProfessionalTypes().subscribe((result: any) => {
+    this.response.ProfessionalTypes().subscribe((result:any)=>{
       this.signUpPropertytype = result.data;
       console.log(this.signUpPropertytype)
 
@@ -37,30 +37,30 @@ export class SignupComponent implements OnInit {
     PhoneNumber: new FormControl("", Validators.required),
     Password: new FormControl("", [Validators.required, Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&]).{8,}')])
   });
-  get validate() {
+  get validate(){
     return this.signup.controls;
   }
   public showPassword: boolean = false;
   checkLength() {
-    let temp: any = this.signup.value.PhoneNumber;
-    if (temp.toString().length > 12) {
+    let temp:any = this.signup.value.PhoneNumber;
+    if(temp.toString().length > 12) {
       alert("Max length allowes is 12");
       this.signup.patchValue({
-        PhoneNumber: temp.toString().slice(0, -1)
+        PhoneNumber: temp.toString().slice(0,-1)
       })
     }
   }
   ngOnInit() {
-    $(document).ready(function () {
-      $('.dropdown-toggle').click(function () {
-        $(this).next().toggleClass('active');
+    $(document).ready(function(){
+      $('.dropdown-toggle').click(function(){
+      $(this).next().toggleClass('active');
       });
+  });
+  $(document).ready(function(){
+    $('.info-toogle').click(function(){
+    $(this).parent().toggleClass('active');
     });
-    $(document).ready(function () {
-      $('.info-toogle').click(function () {
-        $(this).parent().toggleClass('active');
-      });
-    });
+});
   }
   public togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
@@ -68,10 +68,10 @@ export class SignupComponent implements OnInit {
 
   ProceedSignUp() {
     this.submitted = true;
-    if (this.signup.controls["Email"].invalid) {
+    if(this.signup.controls["Email"].invalid) {
       alert("Please Enter a valid Email");
       return
-    } else if (this.signup.controls["Password"].invalid) {
+    } else if(this.signup.controls["Password"].invalid) {
       alert("Password does not meet the required pattern");
       return
     }
@@ -80,18 +80,18 @@ export class SignupComponent implements OnInit {
     }
     if (this.signup.valid) {
       this.service.ProceedSignUp(this.signup.value).subscribe(result => {
-        if (result != null) {
+        if(result!=null ){
           this.responsedata = result;
-          if (this.responsedata.data !== undefined) {
+          if(this.responsedata.data !== undefined){
             // this.responsedata.data = this.responsedata.data;
-            localStorage.setItem('token', JSON.stringify(this.responsedata.data.refreshToken))
-            localStorage.setItem('user', JSON.stringify(this.responsedata.data))
+            localStorage.setItem('token',JSON.stringify(this.responsedata.data.refreshToken))
+            localStorage.setItem('user',JSON.stringify(this.responsedata.data))
             this.notifyService.showSuccess(this.responsedata.message, "");
             this.route.navigate([''])
-          } else {
+          }else{
             this.notifyService.showError(this.responsedata.error.value, "");
           }
-        } else {
+        }else{
           this.notifyService.showError("Unable to signup", "");
         }
         // this.route.navigate([''])
