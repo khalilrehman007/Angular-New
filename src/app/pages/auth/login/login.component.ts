455326ce1ec18e3706371d5fada0c35dbde0330f
luqman.webs <luqmandxb@hotmail.com>
2022-08-26 23:52:36 +0500

    login fixed

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,10 +1,10 @@
-import { Component,DoCheck, ElementRef,ViewChild, OnInit } from '@angular/core';
+import { Component, DoCheck, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { SecondHeaderComponent } from '../../../second-header/second-header.component';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/service/auth.service';
 import { Router } from '@angular/router';
 import { NotificationService } from 'src/app/service/notification.service';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
 import { AppService } from 'src/app/service/app.service';
 
 @Component({
@@ -26,14 +26,13 @@ export class LoginComponent implements OnInit {
   auth2: any;
   @ViewChild('loginRef', { static: true }) loginElement!: ElementRef;
 
-  constructor(private service: AuthService,private route:Router,private notifyService : NotificationService,private _location: Location, private api: AppService) {
-    localStorage.clear();
+  constructor(private service: AuthService, private route: Router, private notifyService: NotificationService, private _location: Location, private api: AppService) {
   }
   Login = new FormGroup({
     Email: new FormControl("", Validators.required),
     Password: new FormControl("", Validators.required)
   });
-  get validate(){
+  get validate() {
     return this.Login.controls;
   }
 
@@ -50,21 +49,26 @@ export class LoginComponent implements OnInit {
       return;
     }
     if (this.Login.valid) {
-      this.service.ProceedLogin(this.Login.value).subscribe(result => {
-        if(result!=null ){
+      let data: any = {};
+      data.DeviceId = localStorage.getItem("deviceToken");
+      data.Email = this.Login.value.Email;
+      data.Password = this.Login.value.Password;
+      this.service.ProceedLogin(data).subscribe(result => {
+        if (result != null) {
           this.responsedata = result
-          if(this.responsedata.data !== null){
-            localStorage.setItem('token',JSON.stringify(this.responsedata.data.refreshToken))
-            localStorage.setItem('user',JSON.stringify(this.responsedata.data))
+          if (this.responsedata.data !== null) {
+            localStorage.clear();
+            localStorage.setItem('token', JSON.stringify(this.responsedata.data.refreshToken))
+            localStorage.setItem('user', JSON.stringify(this.responsedata.data))
             this.notifyService.showSuccess(this.responsedata.message, "");
             this._location.back();
-          }else{
-            if(this.responsedata.error.length > 0){
+          } else {
+            if (this.responsedata.error.length > 0) {
               this.notifyService.showError(this.responsedata.error[0], "");
             }
             this.notifyService.showError(this.responsedata.message, "");
           }
-        }else{
+        } else {
           this.notifyService.showError("Unable to signup", "");
         }
         // this.route.navigate([''])
@@ -77,14 +81,14 @@ export class LoginComponent implements OnInit {
         console.log(googleAuthUser.getAuthResponse());
         //Print profile details in the console logs
         let profile = googleAuthUser.getBasicProfile();
-        let temp:any = "";
-        if(localStorage.getItem("deviceToken")) {
+        let temp: any = "";
+        if (localStorage.getItem("deviceToken")) {
           temp = localStorage.getItem("deviceToken");
         }
-        this.api.Googlelogin({"Provider":"google","IdToken":googleAuthUser.getAuthResponse().id_token, "DeviceId": temp}).subscribe((result:any) => {
-          if(result.message == "You are successfully logged in") {
-            localStorage.setItem('token',JSON.stringify(result.data.refreshToken))
-            localStorage.setItem('user',JSON.stringify(result.data))
+        this.api.Googlelogin({ "Provider": "google", "IdToken": googleAuthUser.getAuthResponse().id_token, "DeviceId": temp }).subscribe((result: any) => {
+          if (result.message == "You are successfully logged in") {
+            localStorage.setItem('token', JSON.stringify(result.data.refreshToken))
+            localStorage.setItem('user', JSON.stringify(result.data))
             this.notifyService.showSuccess(result.message, result.message);
             this._location.back();
           }
@@ -99,7 +103,7 @@ export class LoginComponent implements OnInit {
       (<any>window)['gapi'].load('auth2', () => {
         this.auth2 = (<any>window)['gapi'].auth2.init({
           client_id: '406110889808-nqqu9i00qptmjfi96ore1ee7qo6266jj.apps.googleusercontent.com',
-          plugin_name:'login',
+          plugin_name: 'login',
           cookiepolicy: 'single_host_origin',
           scope: 'profile email'
         });
