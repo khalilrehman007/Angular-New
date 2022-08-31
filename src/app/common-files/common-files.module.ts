0d0292abf250e0fb19293626bf9ca8c41f0c4f76
roshaan9035 <shanijutt9035@gmail.com>
2022-08-31 21:58:07 +0500

    Blog section

--- src/app/common-files/common-files.module.ts
+++ src/app/common-files/common-files.module.ts
@@ -5,13 +5,6 @@ import { RouterModule } from '@angular/router';
 import { CommonFilesRoutingModule } from './common-files-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatIconModule } from '@angular/material/icon';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
 import { CookiePolicyComponent } from './cookie-policy/cookie-policy.component';
 import { TermConditionsComponent } from './term-conditions/term-conditions.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
@@ -21,6 +14,14 @@ import { ContactComponent } from './contact/contact.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { InvestorsComponent } from './investors/investors.component';
 import { FaqComponent } from './faq/faq.component';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+
 
 
 @NgModule({
