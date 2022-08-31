fe1be113cf557f95b400b483858b676f3ed75699
roshaan9035 <shanijutt9035@gmail.com>
2022-09-01 01:54:42 +0500

    Blog category section moduleing done

---
+++ src/app/blog/newsblog/newsblog.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/service/app.service';
+
+
+@Component({
+  selector: 'app-newsblog',
+  templateUrl: './newsblog.component.html',
+  styleUrls: ['./newsblog.component.scss']
+})
+export class NewsblogComponent implements OnInit {
+
+  exploreimg = '../../../assets/images/Blog-Tile.png'
+  blogs: any;
+  newsBlog: any;
+  constructor(private service:AppService) {
+    this.service.BlogCategorybyId(2).subscribe((result:any)=> {
+      this.newsBlog = result.data;
+    })
+  }
+
+  ngOnInit(): void {
+  }
+  status: boolean = false;
+  clickEvent(){
+      this.status = !this.status;       
+  }
+
+}
