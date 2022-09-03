e3f0e6c69f268853979cdc992e2aa1a86b1657da
pankaj sharma <pankajdxba@hotmail.com>
2022-09-03 13:04:26 +0530

    Design Changes

---
+++ src/app/community-search-module/community-search-component/community-search-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-community-search-component',
+  templateUrl: './community-search-component.component.html',
+  styleUrls: ['./community-search-component.component.scss']
+})
+export class CommunitySearchComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/service-search-module/service-search-module.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ServiceSearchComponentComponent } from './service-search-component/service-search-component.component';
+
+
+
+@NgModule({
+  declarations: [
+    ServiceSearchComponentComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ServiceSearchModuleModule { }
