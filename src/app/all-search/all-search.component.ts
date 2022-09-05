bd3626be3cd4299dcf550f5c183579efc3fa112f
pankaj sharma <pankajdxba@hotmail.com>
2022-09-05 22:14:03 +0530

    merge code

---
+++ src/app/all-search/all-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-search',
+  templateUrl: './all-search.component.html',
+  styleUrls: ['./all-search.component.scss']
+})
+export class AllSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/classified-module/classified-module.module.ts
+++ src/app/classified-module/classified-module.module.ts
@@ -4,19 +4,49 @@ import { ClassifiedModuleRoutingModule } from './classified-module-routing.modul
 import { ClassifiedHomeComponent } from './classified-home/classified-home.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { CommonFilesModule } from '../common-files/common-files.module';
+import { MotorModuleModule } from '../motor-module/motor-module.module';
+import { ServiceSearchModuleModule } from '../service-search-module/service-search-module.module';
+import { CommunitySearchModuleModule } from '../community-search-module/community-search-module.module';
+import {MatTabsModule} from '@angular/material/tabs';
+import { AllSearchComponent } from '../all-search/all-search.component';
+import { JobSearchComponent } from '../job-search/job-search.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { NgxSliderModule } from "@angular-slider/ngx-slider";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    ClassifiedHomeComponent
+    ClassifiedHomeComponent,
+    AllSearchComponent,
+    JobSearchComponent
   ],
   imports: [
     CommonModule,
     ClassifiedModuleRoutingModule,
     CarouselModule,
-    CommonFilesModule
+    CommonFilesModule,
+    MotorModuleModule,
+    ServiceSearchModuleModule,
+    CommunitySearchModuleModule,
+    MatTabsModule,
+    MatIconModule,
+    MatAutocompleteModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    NgxSliderModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
-    ClassifiedHomeComponent
+    ClassifiedHomeComponent,
+    AllSearchComponent,
+    JobSearchComponent
   ]
 })
 export class ClassifiedModuleModule { }
