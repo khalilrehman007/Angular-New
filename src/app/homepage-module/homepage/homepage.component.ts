a7d3e0976660ba555c90ca32d11845456ca93fad
luqman.webs <luqman.dxb@hotmail.com>
2022-09-15 17:04:56 +0500

    homepage slider fixed

--- src/app/homepage-module/homepage/homepage.component.ts
+++ src/app/homepage-module/homepage/homepage.component.ts
@@ -238,7 +238,7 @@ export class HomepageComponent implements OnInit {
   ]
 
   customOptions: OwlOptions = {
-    loop: true,
+    loop: false,
     mouseDrag: true,
     touchDrag: true,
     pullDrag: true,
