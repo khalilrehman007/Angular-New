9d8a78295f44386f877bcacbb8a850ad814306cd
roshaan9035 <shanijutt9035@gmail.com>
2022-09-19 17:55:51 +0500

    CMS Contact us page done

--- src/app/ar/common-files/common-files.module.ts
+++ src/app/ar/common-files/common-files.module.ts
@@ -3,6 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { CommonFilesRoutingModule } from './common-files-routing.module';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CookiePolicyComponent } from './cookie-policy/cookie-policy.component';
+import { TermConditionsComponent } from './term-conditions/term-conditions.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InvestorsComponent } from './investors/investors.component';
+import { FaqComponent } from './faq/faq.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,8 +20,6 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
-import { FooterComponent } from './footer/footer.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
 
 
 
@@ -19,7 +27,14 @@ import { AboutusComponent } from './aboutus/aboutus.component';
   declarations: [
     HeaderComponent,
     FooterComponent,
+    CookiePolicyComponent,
+    TermConditionsComponent,
+    PrivacyPolicyComponent,
     AboutusComponent,
+    HowItWorksComponent,
+    ContactComponent,
+    InvestorsComponent,
+    FaqComponent
   ],
   imports: [
     CommonModule,
@@ -31,6 +46,7 @@ import { AboutusComponent } from './aboutus/aboutus.component';
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
+    ReactiveFormsModule,
   ],
   exports: [
     HeaderComponent,
---
+++ src/app/ar/common-files/contact/contact.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import { AppService } from 'src/app/service/app.service';
+import { AuthService } from 'src/app/service/auth.service';
+import { NotificationService } from 'src/app/service/notification.service';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+  image:any;
+  title:any;
+  subHeading:any;
+
+  conductList = [
+    {
+      value: 'Summary valuation  Certificate report',
+      text: "Summary Report"
+    },
+    {
+      value: 'Detailed  valuation  Certificate report',
+      text: "Detailed Report"
+    },
+  ]
+  conductReport:string = "";
+  constructor(private service: AuthService,private route:Router,private notifyService : NotificationService, private api:AppService) {
+    $(window).scrollTop(0);
+    this.api.TermsCondition().subscribe((result:any)=> {
+      this.title = result.data.pageCaptionHelightAr;
+      this.subHeading = result.data.pageCaptionTextAr;
+      this.image = "https://beta.ovaluate.com/" + result.data.fileUrl;
+      this.image = this.image.replaceAll("\\", "/");
+      $(".inner-page-banner-sec").css({"background-image":"url('"+this.image+"')"})
+      $(".inner-banner-heading p").append(this.subHeading);
+    })
+  }
+
+  query: number = -1;
+  onQuerySelect(e: any) {
+    this.conductReport = e.value;
+  }
+  Form = new FormGroup({
+    Name: new FormControl("", Validators.required),
+    Phone: new FormControl("", Validators.required),
+    Email: new FormControl("  ", [Validators.required,Validators.email]),
+  });
+  get name(){
+    return this.Form.get("Name");
+  }
+  get phone(){
+    return this.Form.get("Phone");
+  }
+  get email(){
+    return this.Form.get("Email");
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if(this.conductReport == "" || this.Form.invalid) {
+      alert("Please select all required Fields");
+      return;
+    }
+    let temp:any = {"Name": this.Form.value.Name, "Phone":this.Form.value.Phone,"Email": this.Form.value.Phone, "Query": this.conductReport};
+    this.api.AddContactUs(temp).subscribe((result : any) => {
+      if( result.message == "Your Request  submitted successfully"){
+        alert("Messege sent sucessfully.")
+      }
+      else {
+        alert("Something went wrong.")
+      }
+    })
+  }
+}
