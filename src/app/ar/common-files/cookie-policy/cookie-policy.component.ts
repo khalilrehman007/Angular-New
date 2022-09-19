6f96f210e0e3afa80aaee8abf3f55b4a41f29a2f
roshaan9035 <shanijutt9035@gmail.com>
2022-09-19 18:04:54 +0500

    CMS cookie policy page ar done

--- src/app/ar/common-files/cookie-policy/cookie-policy.component.ts
+++ src/app/ar/common-files/cookie-policy/cookie-policy.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from 'src/app/service/app.service';
+
 
 @Component({
   selector: 'app-cookie-policy',
@@ -6,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cookie-policy.component.scss']
 })
 export class CookiePolicyComponent implements OnInit {
-
-  constructor() { }
+  image:any;
+  text:any;
+  title:any;
+  subHeading:any;
+  constructor(private api: AppService) {
+    $(window).scrollTop(0);
+    this.api.CookiePolicy().subscribe((result:any) => {
+      this.title = result.data.pageCaptionHelightAr;
+      this.subHeading = result.data.pageCaptionTextAr;
+      this.image = "https://beta.ovaluate.com/" + result.data.fileUrl;
+      this.image = this.image.replaceAll("\\", "/");
+      $(".inner-page-banner-sec").css({"background-image":"url('"+this.image+"')"})
+      $(".cms_content-paragraph").append(result.data.pageContentAr);
+      $(".inner-banner-heading p").append(this.subHeading);
+    })
+  }
 
   ngOnInit(): void {
   }
