eb2f509c46aa552724ea03362d164130c57f286f
pankaj sharma <pankajdxba@hotmail.com>
2022-09-19 16:04:07 +0530

    Design Changes

--- src/app/ar/common-files/header/header.component.ts
+++ src/app/ar/common-files/header/header.component.ts
@@ -44,6 +44,7 @@ export class HeaderComponent implements OnInit {
   activeTabRent: any = false;
   activeTabBlog: any = false;
   activeTabArea: any = false;
+  activeShortterm :any = false;
   constructor(private router: Router, private activeRoute: ActivatedRoute, private authService: AuthService, private route: Router, private notifyService: NotificationService, private modalService: NgbModal, private service: AppService, private db: AngularFireDatabase) {
     let a: any = this.currentDate;
     a = a.toString().split(" ")[5].split("T")[1];
@@ -191,6 +192,12 @@ export class HeaderComponent implements OnInit {
       class: 'nav-items classified',
       text: 'Classified',
       link: 'classified',
+    },
+    {
+      src: '../../../assets/images/icons/wallet.svg',
+      class: 'nav-items my-wallet',
+      text: 'My Wallet',
+      link: 'profile',
     }
   ]
 
