0b8eef8b9acb90ad0f79c8946a4e461895690fd3
roshaan9035 <shanijutt9035@gmail.com>
2022-09-22 16:36:37 +0500

    SignUp arabic version done

--- src/app/ar/ar-routing.module.ts
+++ src/app/ar/ar-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path:"", loadChildren:()=>import('./homepage-module/homepage-module.module').then(module => module.HomepageModuleModule)},
   {path:"login", loadChildren:()=>import('./login/login.module').then(module => module.LoginModule)},
+  {path:"signup", loadChildren:()=>import('./signup/signup.module').then(module => module.SignupModule)},
   {path:"blog", loadChildren:()=>import('./blog/blog.module').then(module => module.BlogModule)},
   {path:"explore", loadChildren:()=>import('./explore/explore.module').then(module => module.ExploreModule)},
 
