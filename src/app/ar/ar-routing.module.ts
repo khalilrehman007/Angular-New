cdf53629a9eef5922e71898c05db522fc12661d8
roshaan9035 <shanijutt9035@gmail.com>
2022-09-23 18:36:41 +0500

    Profile Dashboard arabic wip

--- src/app/ar/ar-routing.module.ts
+++ src/app/ar/ar-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../shared/auth.guard';
 
 const routes: Routes = [
-  {path:"", loadChildren:()=>import('./homepage-module/homepage-module.module').then(module => module.HomepageModuleModule)},
-  {path:"login", loadChildren:()=>import('./login/login.module').then(module => module.LoginModule)},
-  {path:"signup", loadChildren:()=>import('./signup/signup.module').then(module => module.SignupModule)},
-  {path:"forgot", loadChildren:()=>import('./forget/forget.module').then(module => module.ForgetModule)},
-  {path:"blog", loadChildren:()=>import('./blog/blog.module').then(module => module.BlogModule)},
-  {path:"explore", loadChildren:()=>import('./explore/explore.module').then(module => module.ExploreModule)},
-  {path:"property", loadChildren:()=>import('./property/property.module').then(module => module.PropertyModule)},
-  {path:"sellrent", loadChildren:()=>import('./sell/sell.module').then(module => module.SellModule)},
+  { path: "", loadChildren: () => import('./homepage-module/homepage-module.module').then(module => module.HomepageModuleModule) },
+  { path: "login", loadChildren: () => import('./login/login.module').then(module => module.LoginModule) },
+  { path: "signup", loadChildren: () => import('./signup/signup.module').then(module => module.SignupModule) },
+  { path: "forgot", loadChildren: () => import('./forget/forget.module').then(module => module.ForgetModule) },
+  { path: "profile", loadChildren: () => import('./dashboard/dashboard.module').then(module => module.DashboardModule), canActivate: [AuthGuard] },
+  { path: "blog", loadChildren: () => import('./blog/blog.module').then(module => module.BlogModule) },
+  { path: "explore", loadChildren: () => import('./explore/explore.module').then(module => module.ExploreModule) },
+  { path: "property", loadChildren: () => import('./property/property.module').then(module => module.PropertyModule) },
+  { path: "sellrent", loadChildren: () => import('./sell/sell.module').then(module => module.SellModule) },
+  { path: "find-agent", loadChildren: () => import('./agent-and-companies/agent-and-companies.module').then(module => module.AgentAndCompaniesModule) },
+  { path: "find-companies", loadChildren: () => import('./agent-and-companies/agent-and-companies.module').then(module => module.AgentAndCompaniesModule) },
+  // { path: "agent-details/:id", loadChildren: () => import('./agent-details/agent-details.module').then(module => module.AgentDetailsModule) },
+  // { path: "company-details/:id", loadChildren: () => import('./company-details/company-details.module').then(module => module.CompanyDetailsModule) },
 ];
 
 @NgModule({
---
+++ src/app/ar/dashboard/dashboard.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CommonFilesModule } from '../common-files/common-files.module';
+import { LoaderModule } from 'src/app/loader/loader.module';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { MatIconModule } from '@angular/material/icon';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    CommonFilesModule,
+    LoaderModule,
+    
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgxPaginationModule,
+    MatIconModule,
+    NgbModule,
+    MatSlideToggleModule
+  ]
+})
+export class DashboardModule { }
