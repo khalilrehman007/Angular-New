4d41e786e190537aa0dcccca9a70cbe3db1baf92
luqman.webs <luqman.dxb@hotmail.com>
2022-09-23 16:04:52 +0500

    property filter arabic done

--- src/app/ar/property/property-routing.module.ts
+++ src/app/ar/property/property-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PropertyDetailComponent } from './property-detail/property-detail.component';
-// import { SearchComponent } from './search/search.component';
+import { SearchComponent } from './search/search.component';
 // import { ViewmapComponent } from './viewmap/viewmap.component';
 // import { ShortTermRentComponent } from './short-term-rent/short-term-rent.component';
 
 const routes: Routes = [
-  // { path: "search", component: SearchComponent },
+  { path: "search", component: SearchComponent },
   // { path: "search/:type", component: SearchComponent },
   { path: "detail", component: PropertyDetailComponent },
   // { path: "mapview", component: ViewmapComponent },
