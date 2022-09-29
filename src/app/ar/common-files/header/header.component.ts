4047271aa8fbfcff087e6333a43a4c9c1ad1ca3b
roshaan9035 <shanijutt9035@gmail.com>
2022-09-29 12:39:03 +0500

    Header and property filter route fixed`

--- src/app/ar/common-files/header/header.component.ts
+++ src/app/ar/common-files/header/header.component.ts
@@ -150,7 +150,7 @@ export class HeaderComponent implements OnInit {
       src: '../../../assets/images/icons/login.svg',
       class: 'nav-items sign-in',
       text: this.loggedInUser == null ? 'تسجيل الدخول' : JSON.parse(this.loggedInUser).fullName,
-      link: this.loggedInUser == null ? 'login' : 'profile',
+      link: this.loggedInUser == null ? '/ar/login' : '/ar/profile',
     },
     {
       src: '../../../assets/images/icons/ioi.svg',
