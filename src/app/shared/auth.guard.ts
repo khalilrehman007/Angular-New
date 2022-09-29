08bbd2b7684773c3b073defa6444882a185d2f09
roshaan9035 <shanijutt9035@gmail.com>
2022-09-29 16:31:05 +0500

    Routing wip

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -7,23 +7,23 @@ import {
   RouterStateSnapshot, RoutesRecognized,
   UrlTree
 } from '@angular/router';
-import {filter, Observable, pairwise} from 'rxjs';
+import { filter, Observable, pairwise } from 'rxjs';
 import { AuthService } from '../service/auth.service';
-import {NotificationService} from "../service/notification.service";
-import {AppService} from "../service/app.service";
-import {Location} from "@angular/common";
+import { NotificationService } from "../service/notification.service";
+import { AppService } from "../service/app.service";
+import { Location } from "@angular/common";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
   previousUrl: any;
-  routeChange :any;
-  constructor(public appService :AppService ,private service:AuthService,private route:Router,private notifyService : NotificationService){
+  routeChange: any;
+  constructor(public appService: AppService, private service: AuthService, private route: Router, private notifyService: NotificationService) {
 
   }
 
-  canActivate(){
+  canActivate() {
     // if(this.service.IsLoggedIn()){
     // return true;
     // }else{
@@ -33,7 +33,20 @@ export class AuthGuard implements CanActivate {
 
     if (!this.service.isAuthenticated()) {
       this.notifyService.showError("You need to register/login", "");
-      this.route.navigate(['login']);
+      this.route.events.subscribe((val: any) => {
+        if (val instanceof NavigationEnd) {
+          let temp: any = val.url.split("/");
+          if (temp[1] == "ar") {
+            this.route.navigate(['/ar/login']);
+            $("html").attr("dir", "rtl");
+            $("title").text("OV- المتجر");
+          } else {
+            this.route.navigate(['login']);
+            $("html").removeAttr("dir");
+            $("title").text("OV - Marketplace");
+          }
+        }
+      })
       return false;
     }
 
