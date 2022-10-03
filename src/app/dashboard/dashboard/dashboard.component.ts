333d4e324e1d3cb1c14592996fbd661612a34dda
pankaj sharma <pankajdxba@hotmail.com>
2022-10-03 16:51:10 +0530

    Design Code Merge

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1057,6 +1057,9 @@ export class DashboardComponent implements OnInit {
   PaymentPopup(PaymentPopupModal:any) {
     this.modalService.open(PaymentPopupModal, { centered: true });
   }
+  TransferPopup(TransferModal:any) {
+    this.modalService.open(TransferModal, { centered: true });
+  }
 
   buyCount: any;
   rentCount: any;
