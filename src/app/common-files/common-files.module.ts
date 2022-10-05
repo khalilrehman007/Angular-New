8373f16aef01125f692b3c6755406a2f6d1ec396
pankaj sharma <pankajdxba@hotmail.com>
2022-10-05 17:59:34 +0530

    Design Changes

--- src/app/common-files/common-files.module.ts
+++ src/app/common-files/common-files.module.ts
@@ -22,6 +22,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { AlertsModule } from '../alerts/alerts.module';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 
 @NgModule({
@@ -47,6 +48,7 @@ import { AlertsModule } from '../alerts/alerts.module';
     MatFormFieldModule,
     MatSelectModule,
     MatInputModule,
+    MatAutocompleteModule,
     ReactiveFormsModule,
     AlertsModule,
     NgbModule
