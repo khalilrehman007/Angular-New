92a91dd7179a7a92a1e9c303a2bfcb30d1de21dc
luqman.webs <luqman.dxb@hotmail.com>
2022-10-12 17:27:23 +0500

    da wip

--- src/app/transaction-data-module/transaction-data/transaction-data.component.ts
+++ src/app/transaction-data-module/transaction-data/transaction-data.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit, ElementRef, ViewChild,AfterViewInit } from '@angular/core';
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {FormControl} from '@angular/forms';
-import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
-import {MatChipInputEvent} from '@angular/material/chips';
-import {Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { Options } from '@angular-slider/ngx-slider';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { CookieService } from 'ngx-cookie-service';
+import { AppService } from 'src/app/service/app.service';
 //  Data Table
 export interface UserData {
   id: string;
@@ -56,9 +58,9 @@ const NAMES: string[] = [
 export class TransactionDataComponent implements OnInit {
 
 
-//  Data Table
-displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-dataSource: MatTableDataSource<UserData>;
+  //  Data Table
+  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+  dataSource: MatTableDataSource<UserData>;
 
 
   @ViewChild(MatPaginator)
@@ -83,133 +85,128 @@ dataSource: MatTableDataSource<UserData>;
     }
   };
 
-    //Price RangeSlider
+  //Price RangeSlider
 
-    PriceminValue: number = 10;
-    PricemaxValue: number = 1000000;
-    Priceoptions: Options = {
-      floor: 10,
-      ceil: 1000000,
-      translate: (value: number): string => {
-        return value + 'AED';
-      }
-    };
+  PriceminValue: number = 10;
+  PricemaxValue: number = 1000000;
+  Priceoptions: Options = {
+    floor: 10,
+    ceil: 1000000,
+    translate: (value: number): string => {
+      return value + 'AED';
+    }
+  };
 
 
   separatorKeysCodes: number[] = [ENTER, COMMA];
   CommunityCtrl = new FormControl('');
-  filteredcommunity: Observable<string[]>;
-  communityfield: string[] = ['Dubai Community'];
+  filteredcommunity: any = [];
+  communityfield: any= [];
   allcommunityfield: string[] = ['Dubai Community', 'Dubai Community', 'Dubai Community'];
 
   // Property Type Filter
 
   PropertyTypCtrl = new FormControl('');
-  filteredProperty: Observable<string[]>;
+  filteredProperty: any = [];
   ProTypefield: string[] = ['Dubai'];
   allProTypefield: string[] = ['Property Type', 'Dubai', 'Dubai'];
 
   // Property Filter
 
   PropertyCtrl = new FormControl('');
-  filteredPropertyOnly: Observable<string[]>;
+  filteredPropertyOnly: any = [];
   Profield: string[] = ['Dubai'];
   allProfield: string[] = ['Property', 'Dubai', 'Dubai'];
 
-   // Transaction Type Filter
+  // Transaction Type Filter
 
-   TransactionCtrl = new FormControl('');
-   filteredTransaction: Observable<string[]>;
-   Transactionfield: string[] = ['Dubai'];
-   allTransactionfield: string[] = ['Transaction', 'Dubai', 'Dubai'];
+  TransactionCtrl = new FormControl('');
+  filteredTransaction: Observable<string[]>;
+  Transactionfield: string[] = ['Dubai'];
+  allTransactionfield: string[] = ['Transaction', 'Dubai', 'Dubai'];
 
 
-    // Sales Sequences Filter
+  // Sales Sequences Filter
 
-    salesCtrl = new FormControl('');
-    filteredsales: Observable<string[]>;
-    salesfield: string[] = ['Dubai'];
-    allsalesfield: string[] = ['Sale', 'Dubai', 'Dubai'];
+  salesCtrl = new FormControl('');
+  filteredsales: Observable<string[]>;
+  salesfield: string[] = ['Dubai'];
+  allsalesfield: string[] = ['Sale', 'Dubai', 'Dubai'];
 
-    // Developers Filter
+  // Developers Filter
 
-    DevelopersCtrl = new FormControl('');
-    filteredDevelopers: Observable<string[]>;
-    Developersfield: string[] = ['All'];
-    allDevelopersfield: string[] = ['All Developers', 'Developers', 'Developers'];
+  DevelopersCtrl = new FormControl('');
+  filteredDevelopers: Observable<string[]>;
+  Developersfield: string[] = ['All'];
+  allDevelopersfield: string[] = ['All Developers', 'Developers', 'Developers'];
 
-    // Bedrooms Filter
+  // Bedrooms Filter
 
-    bedsCtrl = new FormControl('');
-    filteredbeds: Observable<string[]>;
-    bedsfield: string[] = ['All'];
-    allbedsfield: string[] = ['All Developers', 'Developers', 'Developers'];
+  bedsCtrl = new FormControl('');
+  filteredbeds: Observable<string[]>;
+  bedsfield: string[] = ['All'];
+  allbedsfield: string[] = ['All Developers', 'Developers', 'Developers'];
 
-    // City Filter
+  // City Filter
 
-    CityCtrl = new FormControl('');
-    filteredCity: Observable<string[]>;
-    Cityfield: string[] = ['City'];
-    allCityfield: string[] = ['All City', 'City', 'City'];
+  CityCtrl = new FormControl('');
+  filteredCity: Observable<string[]>;
+  Cityfield: any = [];
+  allCityfield: string[] = ['All City', 'City', 'City'];
 
-  @ViewChild('ComunityInput') ComunityInput:any;
-  @ViewChild('PropertyTypeInput') PropertyTypeInput:any;
-  @ViewChild('PropertyInput') PropertyInput:any;
-  @ViewChild('TransactionInput') TransactionInput:any;
-  @ViewChild('SalesInput') SalesInput:any;
-  @ViewChild('developersInput') developersInput:any;
-  @ViewChild('bedInput') bedInput:any;
-  @ViewChild('CityInput') CityInput:any;
+  @ViewChild('ComunityInput') ComunityInput: any;
+  @ViewChild('PropertyTypeInput') PropertyTypeInput: any;
+  @ViewChild('PropertyInput') PropertyInput: any;
+  @ViewChild('TransactionInput') TransactionInput: any;
+  @ViewChild('SalesInput') SalesInput: any;
+  @ViewChild('developersInput') developersInput: any;
+  @ViewChild('bedInput') bedInput: any;
+  @ViewChild('CityInput') CityInput: any;
 
-  constructor() {
-    // Community Type Filter
-    this.filteredcommunity = this.CommunityCtrl.valueChanges.pipe(
+  countryData: any = "";
+  citiesData:any = "";
+
+  constructor(private cookie: CookieService, private service: AppService) {
+    this.countryData = JSON.parse(this.cookie.get("countryData"));
+    this.service.FindCities({ "CountryId": this.countryData.id, "Locations": [] }).subscribe((result:any) => {
+      this.citiesData = result.data;
+    })
+    this.service.LoadType(1).subscribe((result:any) => {
+      console.log(result.data);
+    })
+    // Transaction Type filter
+    this.filteredTransaction = this.TransactionCtrl.valueChanges.pipe(
       startWith(null),
-      map((community: string | null) => (community ? this._filter(community) : this.allcommunityfield.slice())),
+      map((transaction: string | null) => (transaction ? this._filter(transaction) : this.allTransactionfield.slice())),
     );
-     // Property Type Filter
-     this.filteredProperty = this.PropertyTypCtrl.valueChanges.pipe(
+    // Sales Sequences filter
+    this.filteredsales = this.salesCtrl.valueChanges.pipe(
       startWith(null),
-      map((protype: string | null) => (protype ? this._filter(protype) : this.allProTypefield.slice())),
+      map((sale: string | null) => (sale ? this._filter(sale) : this.allsalesfield.slice())),
     );
-      // Property Filter
-      this.filteredPropertyOnly = this.PropertyCtrl.valueChanges.pipe(
-        startWith(null),
-        map((property: string | null) => (property ? this._filter(property) : this.allProfield.slice())),
-      );
-      // Transaction Type filter
-      this.filteredTransaction = this.TransactionCtrl.valueChanges.pipe(
-        startWith(null),
-        map((transaction: string | null) => (transaction ? this._filter(transaction) : this.allTransactionfield.slice())),
-      );
-      // Sales Sequences filter
-      this.filteredsales = this.salesCtrl.valueChanges.pipe(
-        startWith(null),
-        map((sale: string | null) => (sale ? this._filter(sale) : this.allsalesfield.slice())),
-      );
-        // Developers filter
-        this.filteredDevelopers = this.DevelopersCtrl.valueChanges.pipe(
-        startWith(null),
-        map((developer: string | null) => (developer ? this._filter(developer) : this.allDevelopersfield.slice())),
-      );
-      // Bed Rooms filter
-      this.filteredbeds = this.bedsCtrl.valueChanges.pipe(
+    // Developers filter
+    this.filteredDevelopers = this.DevelopersCtrl.valueChanges.pipe(
+      startWith(null),
+      map((developer: string | null) => (developer ? this._filter(developer) : this.allDevelopersfield.slice())),
+    );
+    // Bed Rooms filter
+    this.filteredbeds = this.bedsCtrl.valueChanges.pipe(
       startWith(null),
       map((beds: string | null) => (beds ? this._filter(beds) : this.allbedsfield.slice())),
     );
-     // City filter
-     this.filteredCity = this.CityCtrl.valueChanges.pipe(
+    // City filter
+    this.filteredCity = this.CityCtrl.valueChanges.pipe(
       startWith(null),
       map((city: string | null) => (city ? this._filter(city) : this.allCityfield.slice())),
     );
 
     // Data Table
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+    const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
     // Assign the data to the data source for the table to render
     this.dataSource = new MatTableDataSource(users);
   }
- // Community Type Filter
- add(event: MatChipInputEvent): void {
+  // Community Type Filter
+  add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
     if (value) {
       this.communityfield.push(value);
@@ -229,152 +226,163 @@ dataSource: MatTableDataSource<UserData>;
     this.CommunityCtrl.setValue(null);
   }
 
- // Property Type Filter
- add1(event: MatChipInputEvent): void {
-  const value1 = (event.value || '').trim();
-  if (value1) {
-    this.ProTypefield.push(value1);
+  // Property Type Filter
+  add1(event: MatChipInputEvent): void {
+    const value1 = (event.value || '').trim();
+    if (value1) {
+      this.ProTypefield.push(value1);
+    }
+    event.chipInput!.clear();
+    this.PropertyTypCtrl.setValue(null);
   }
-  event.chipInput!.clear();
-  this.PropertyTypCtrl.setValue(null);
-}
-remove1(protype: string): void {
-  const index1 = this.ProTypefield.indexOf(protype);
-  if (index1 >= 0) {
-    this.ProTypefield.splice(index1, 1);
+  remove1(protype: string): void {
+    const index1 = this.ProTypefield.indexOf(protype);
+    if (index1 >= 0) {
+      this.ProTypefield.splice(index1, 1);
+    }
   }
-}
-selected1(event: MatAutocompleteSelectedEvent): void {
-  this.ProTypefield.push(event.option.viewValue);
-  this.PropertyTypeInput.nativeElement.value = '';
-  this.PropertyTypCtrl.setValue(null);
-}
+  selected1(event: MatAutocompleteSelectedEvent): void {
+    this.ProTypefield.push(event.option.viewValue);
+    this.PropertyTypeInput.nativeElement.value = '';
+    this.PropertyTypCtrl.setValue(null);
+  }
 
- // Property Filter
- add2(event: MatChipInputEvent): void {
-  const value2 = (event.value || '').trim();
-  if (value2) {
-    this.Profield.push(value2);
+  // Property Filter
+  add2(event: MatChipInputEvent): void {
+    const value2 = (event.value || '').trim();
+    if (value2) {
+      this.Profield.push(value2);
+    }
+    event.chipInput!.clear();
+    this.PropertyCtrl.setValue(null);
   }
-  event.chipInput!.clear();
-  this.PropertyCtrl.setValue(null);
-}
-remove2(property: string): void {
-  const index2 = this.Profield.indexOf(property);
-  if (index2 >= 0) {
-    this.Profield.splice(index2, 1);
+  remove2(property: string): void {
+    const index2 = this.Profield.indexOf(property);
+    if (index2 >= 0) {
+      this.Profield.splice(index2, 1);
+    }
   }
-}
-selected2(event: MatAutocompleteSelectedEvent): void {
-  this.Profield.push(event.option.viewValue);
-  this.PropertyInput.nativeElement.value = '';
-  this.PropertyCtrl.setValue(null);
-}
+  selected2(event: MatAutocompleteSelectedEvent): void {
+    this.Profield.push(event.option.viewValue);
+    this.PropertyInput.nativeElement.value = '';
+    this.PropertyCtrl.setValue(null);
+  }
 
- // Transaction Type Filter
- add3(event: MatChipInputEvent): void {
-  const value3 = (event.value || '').trim();
-  if (value3) {
-    this.Transactionfield.push(value3);
+  // Transaction Type Filter
+  add3(event: MatChipInputEvent): void {
+    const value3 = (event.value || '').trim();
+    if (value3) {
+      this.Transactionfield.push(value3);
+    }
+    event.chipInput!.clear();
+    this.TransactionCtrl.setValue(null);
   }
-  event.chipInput!.clear();
-  this.TransactionCtrl.setValue(null);
-}
-remove3(transaction: string): void {
-  const index3 = this.Transactionfield.indexOf(transaction);
-  if (index3 >= 0) {
-    this.Transactionfield.splice(index3, 1);
+  remove3(transaction: string): void {
+    const index3 = this.Transactionfield.indexOf(transaction);
+    if (index3 >= 0) {
+      this.Transactionfield.splice(index3, 1);
+    }
   }
-}
-selected3(event: MatAutocompleteSelectedEvent): void {
-  this.Transactionfield.push(event.option.viewValue);
-  this.TransactionInput.nativeElement.value = '';
-  this.TransactionCtrl.setValue(null);
-}
+  selected3(event: MatAutocompleteSelectedEvent): void {
+    this.Transactionfield.push(event.option.viewValue);
+    this.TransactionInput.nativeElement.value = '';
+    this.TransactionCtrl.setValue(null);
+  }
 
- // Sales Sequences Filter
- add4(event: MatChipInputEvent): void {
-  const value4 = (event.value || '').trim();
-  if (value4) {
-    this.salesfield.push(value4);
+  // Sales Sequences Filter
+  add4(event: MatChipInputEvent): void {
+    const value4 = (event.value || '').trim();
+    if (value4) {
+      this.salesfield.push(value4);
+    }
+    event.chipInput!.clear();
+    this.salesCtrl.setValue(null);
   }
-  event.chipInput!.clear();
-  this.salesCtrl.setValue(null);
-}
-remove4(sale: string): void {
-  const index4 = this.salesfield.indexOf(sale);
-  if (index4 >= 0) {
-    this.salesfield.splice(index4, 1);
+  remove4(sale: string): void {
+    const index4 = this.salesfield.indexOf(sale);
+    if (index4 >= 0) {
+      this.salesfield.splice(index4, 1);
+    }
   }
-}
-selected4(event: MatAutocompleteSelectedEvent): void {
-  this.salesfield.push(event.option.viewValue);
-  this.SalesInput.nativeElement.value = '';
-  this.salesCtrl.setValue(null);
-}
+  selected4(event: MatAutocompleteSelectedEvent): void {
+    this.salesfield.push(event.option.viewValue);
+    this.SalesInput.nativeElement.value = '';
+    this.salesCtrl.setValue(null);
+  }
 
- // Sales Sequences Filter
- add5(event: MatChipInputEvent): void {
-  const value5 = (event.value || '').trim();
-  if (value5) {
-    this.Developersfield.push(value5);
+  // Sales Sequences Filter
+  add5(event: MatChipInputEvent): void {
+    const value5 = (event.value || '').trim();
+    if (value5) {
+      this.Developersfield.push(value5);
+    }
+    event.chipInput!.clear();
+    this.DevelopersCtrl.setValue(null);
   }
-  event.chipInput!.clear();
-  this.DevelopersCtrl.setValue(null);
-}
-remove5(developer: string): void {
-  const index5 = this.Developersfield.indexOf(developer);
-  if (index5 >= 0) {
-    this.Developersfield.splice(index5, 1);
+  remove5(developer: string): void {
+    const index5 = this.Developersfield.indexOf(developer);
+    if (index5 >= 0) {
+      this.Developersfield.splice(index5, 1);
+    }
   }
-}
-selected5(event: MatAutocompleteSelectedEvent): void {
-  this.Developersfield.push(event.option.viewValue);
-  this.developersInput.nativeElement.value = '';
-  this.DevelopersCtrl.setValue(null);
-}
+  selected5(event: MatAutocompleteSelectedEvent): void {
+    this.Developersfield.push(event.option.viewValue);
+    this.developersInput.nativeElement.value = '';
+    this.DevelopersCtrl.setValue(null);
+  }
 
- // Bed Rooms Filter
- add6(event: MatChipInputEvent): void {
-  const value6 = (event.value || '').trim();
-  if (value6) {
-    this.bedsfield.push(value6);
+  // Bed Rooms Filter
+  add6(event: MatChipInputEvent): void {
+    const value6 = (event.value || '').trim();
+    if (value6) {
+      this.bedsfield.push(value6);
+    }
+    event.chipInput!.clear();
+    this.bedsCtrl.setValue(null);
   }
-  event.chipInput!.clear();
-  this.bedsCtrl.setValue(null);
-}
-remove6(beds: string): void {
-  const index6 = this.bedsfield.indexOf(beds);
-  if (index6 >= 0) {
-    this.bedsfield.splice(index6, 1);
+  remove6(beds: string): void {
+    const index6 = this.bedsfield.indexOf(beds);
+    if (index6 >= 0) {
+      this.bedsfield.splice(index6, 1);
+    }
   }
-}
-selected6(event: MatAutocompleteSelectedEvent): void {
-  this.bedsfield.push(event.option.viewValue);
-  this.bedInput.nativeElement.value = '';
-  this.bedsCtrl.setValue(null);
-}
+  selected6(event: MatAutocompleteSelectedEvent): void {
+    this.bedsfield.push(event.option.viewValue);
+    this.bedInput.nativeElement.value = '';
+    this.bedsCtrl.setValue(null);
+  }
 
-// City Filter
-add7(event: MatChipInputEvent): void {
-  const value7 = (event.value || '').trim();
-  if (value7) {
-    this.Cityfield.push(value7);
+  // City Filter
+  add7(event: MatChipInputEvent): void {
+    const value7 = (event.value || '').trim();
+    if (value7) {
+      this.Cityfield.push(value7);
+    }
+    event.chipInput!.clear();
+    this.CityCtrl.setValue(null);
   }
-  event.chipInput!.clear();
-  this.CityCtrl.setValue(null);
-}
-remove7(city: string): void {
-  const index7 = this.Cityfield.indexOf(city);
-  if (index7 >= 0) {
-    this.Cityfield.splice(index7, 1);
+  loadDistrict() {
+    this.filteredcommunity = [];
+    for(let i = 0; i < this.Cityfield.length; i++) {
+      this.service.FindDistricts({ "CityId":this.Cityfield[i].id, "Locations" : [] }).subscribe((result:any) => {
+        for(let item of result.data) {
+          this.filteredcommunity.push(item);
+        }
+      })
+    }
   }
-}
-selected7(event: MatAutocompleteSelectedEvent): void {
-  this.Cityfield.push(event.option.viewValue);
-  this.CityInput.nativeElement.value = '';
-  this.CityCtrl.setValue(null);
-}
+  remove7(city: any): void {
+    const index7 = this.Cityfield.indexOf(city);
+    if (index7 >= 0) {
+      this.Cityfield.splice(index7, 1);
+    }
+  }
+  selected7(event: any): void {
+    this.Cityfield.push({"id":event.option.value, "name":event.option.viewValue});
+    this.CityInput.nativeElement.value = '';
+    this.CityCtrl.setValue(null);
+    this.loadDistrict();
+  }
 
   private _filter(value: string): string[] {
     const filtercomunity = value.toLowerCase();
@@ -392,7 +400,7 @@ selected7(event: MatAutocompleteSelectedEvent): void {
     const filterbeds = value.toLowerCase();
     return this.allbedsfield.filter(beds => beds.toLowerCase().includes(filterbeds));
   }
-  
+
 
   ngOnInit(): void {
   }
