dbc1756c2e3331b21945a51659a91267a4ee090d
pankaj sharma <pankajdxba@hotmail.com>
2022-10-15 13:43:19 +0530

    Design Changes

---
+++ src/app/data-intelligence/data/yearly-analysis-residential/yearly-analysis-residential.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-yearly-analysis-residential',
+  templateUrl: './yearly-analysis-residential.component.html',
+  styleUrls: ['./yearly-analysis-residential.component.scss']
+})
+export class YearlyAnalysisResidentialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
