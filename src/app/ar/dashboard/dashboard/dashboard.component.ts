4d313a2246fd55f42aa624f28fa4de86646f3548
pankaj sharma <pankajdxba@hotmail.com>
2022-10-21 17:46:10 +0530

    Design Changes

--- src/app/ar/dashboard/dashboard/dashboard.component.ts
+++ src/app/ar/dashboard/dashboard/dashboard.component.ts
@@ -18,6 +18,7 @@ import { CookieService } from 'ngx-cookie-service';
 export class DashboardComponent implements OnInit, AfterViewInit {
 blogs: any;
   proFrame = '../../../assets/images/profile/pro-img-frame.png'
+  pdf = 'assets/images/icons/pdf.png'
   logoutimg = '../../../assets/images/logout-popup-banner.png'
   proAvatar: any = '../../../assets/images/user.png'
   proClose = '../../../assets/images/profile/close.png'
