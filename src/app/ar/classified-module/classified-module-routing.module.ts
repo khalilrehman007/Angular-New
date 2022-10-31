be0ccb176b7cdf9fe35148ee9d3deb7c5721905f
roshaan9035 <shanijutt9035@gmail.com>
2022-10-31 15:07:37 +0500

    Classified router linked

--- src/app/ar/classified-module/classified-module-routing.module.ts
+++ src/app/ar/classified-module/classified-module-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClassifiedHomeComponent } from './classified-home/classified-home.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { DetailsComponent } from './details/details.component';
+import { MotorsAdDetailsComponent } from './motors-ad-details/motors-ad-details.component';
+import { MotorsAdPaymentComponent } from './motors-ad-payment/motors-ad-payment.component';
 
 const routes: Routes = [
   { path: "", component: ClassifiedHomeComponent },
+  { path: "add-post", component: AddPostComponent },
+  { path: "details", component: DetailsComponent },
+  { path: "motor-ad-details", component: MotorsAdDetailsComponent },
+  { path: "motor-ad-payment", component: MotorsAdPaymentComponent }
 ];
 
 @NgModule({
