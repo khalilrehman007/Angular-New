0fcfdfd0315e2b197c29866d9dc1cfae3cce9d58
roshaan9035 <shanijutt9035@gmail.com>
2022-10-31 13:19:09 +0500

    Arabic buy rent search text changes

--- src/app/ar/rent-search/rent-search/rent-search.component.ts
+++ src/app/ar/rent-search/rent-search/rent-search.component.ts
@@ -241,7 +241,7 @@ export class RentSearchComponent implements OnInit {
       let temp: any = e;
       if (temp.message == "Property Listing Type List fetched successfully") {
         for (let list of temp.data) {
-          this.rentTypes.push({ name: list.name, id: list.id });
+          this.rentTypes.push({ name: list.nameAr, id: list.id });
         }
       }
     });
