d233338e403441badddba68a062c2a80f4944324
roshaan9035 <shanijutt9035@gmail.com>
2022-11-01 00:08:19 +0500

    Luxury project module done

---
+++ src/app/ar/projects/project-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// import { CityComponent } from './city/city.component';
+// import { DetailsComponent } from './details/details.component';
+import { ProjectComponent } from './project/project.component';
+import { LuxuryComponent } from './luxury/luxury.component';
+import { UltraLuxuryComponent } from './ultraluxury/ultraluxury.component';
+import { ScoresComponent } from './scores/scores.component';
+
+const routes: Routes = [
+  { path: "", component: ProjectComponent },
+  { path: "luxury", component: LuxuryComponent },
+  { path: "ultraluxury", component: UltraLuxuryComponent },
+  { path: "scores", component: ScoresComponent },
+  // { path: "city/:id", component: CityComponent },
+  // { path: "details/:id", component: DetailsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProjectRoutingModule { }
---
+++ src/app/ar/projects/project.module.ts
@@ -0,0 +1,79 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProjectRoutingModule } from './project-routing.module';
+import { ProjectComponent } from './project/project.component';
+import { CommonFilesModule } from '../common-files/common-files.module';
+import { BreadcrumbsModule } from '../breadcrumbs/breadcrumbs.module';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+// import { CityComponent } from './city/city.component';
+// import { DetailsComponent } from './details/details.component';
+import { LuxuryComponent } from './luxury/luxury.component';
+import { UltraLuxuryComponent } from './ultraluxury/ultraluxury.component';
+import { ScoresComponent } from './scores/scores.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+// Import ng-circle-progress
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NgxSliderModule } from "@angular-slider/ngx-slider";
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { HttpClientModule } from '@angular/common/http';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ProjectFilterComponent } from './project-filter/project-filter.component';
+
+@NgModule({
+  declarations: [
+    ProjectComponent,
+    // CityComponent,
+    // DetailsComponent,
+    LuxuryComponent,
+    UltraLuxuryComponent,
+    ScoresComponent,
+    ProjectFilterComponent
+  ],
+  imports: [
+    CommonModule,
+    ProjectRoutingModule,
+    CommonFilesModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    BreadcrumbsModule,
+    CarouselModule,
+    NgbModule,
+    NgxPaginationModule,
+    MatTabsModule,
+    NgxSliderModule,
+    NgxGalleryModule,
+    HttpClientModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    // Specify ng-circle-progress as an import
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+      titleFontWeight: "700"
+    })
+  ]
+})
+export class ExploreModule { }
