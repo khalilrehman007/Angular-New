cbf6ca8d6a9cd0934e565d1a85d842e3a2e3ff18
roshaan9035 <shanijutt9035@gmail.com>
2022-11-11 11:32:25 +0500

    compare wip

--- src/app/compare-module/compare-listings/compare-listings.component.ts
+++ src/app/compare-module/compare-listings/compare-listings.component.ts
@@ -19,8 +19,27 @@ export class CompareListingsComponent implements OnInit {
     { img: '/assets/images/pro-comparison.png', price: '350,000', title: 'Blue Wave Tower', address: 'Etihad Tower 4, Etihad Towers, Corniche Road, Abu Dhabi', city: 'Dubai', carpetArea: '1200', bedrooms: '2', bathrooms: '1', parkings: 'No', propertyAge: '10 yeras', floorNo: '10', totalFloor: '25', towerBlock: 'yes', unitNo: '123', furnishingType: 'Furnished' }
   ];
   propertyDetails: any = [];
+  viewPropertyOne: any = "";
+  viewPropertyTwo: any = "";
+  viewPropertyThree: any = "";
 
   constructor(private authService: AuthService, private route: Router, private notifyService: NotificationService, private modalService: NgbModal, private service: AppService) {
+    if (localStorage.getItem("clickProprtyOne")){
+      this.viewPropertyOne = localStorage.getItem("clickProprtyOne");
+      this.viewPropertyOne = JSON.parse(this.viewPropertyOne);
+    }
+    
+    if (localStorage.getItem("clickProprtyTwo")){
+      this.viewPropertyTwo = localStorage.getItem("clickProprtyTwo");
+      this.viewPropertyTwo = JSON.parse(this.viewPropertyTwo);
+    }
+    
+    if (localStorage.getItem("clickProprtyThree")){
+      this.viewPropertyThree = localStorage.getItem("clickProprtyThree");
+      this.viewPropertyThree = JSON.parse(this.viewPropertyThree);
+      console.log(this.viewPropertyThree)
+    }
+    
     this.service.ComparableProperties([{ "Id": "31" }, { "Id": "32" }, { "Id": "33" }]).subscribe((result: any) => {
       this.propertyDetails = result.data;
       console.log(this.propertyDetails)
