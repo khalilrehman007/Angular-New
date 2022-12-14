4a3437320ecf6d4cab46285ab79938b6631555fe
pankaj sharma <pankajdxba@hotmail.com>
2022-12-15 00:20:58 +0530

    Design Changes

--- src/app/classified-module/classified-listings/motors-filters/motors-filters.component.ts
+++ src/app/classified-module/classified-listings/motors-filters/motors-filters.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Select2 } from 'select2';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-motors-filters',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./motors-filters.component.scss']
 })
 export class MotorsFiltersComponent implements OnInit {
-
-  constructor() { }
+  minValue: number;
+  maxValue: number;
+  options: any = {};
+  constructor(private modalService: NgbModal) { 
+    this.minValue = 1000;
+    this.maxValue = 50000000;
+  }
 
   ngOnInit(): void {
   }
-
+  status: boolean = false;
+  status1: boolean = false;
+  clickEvent1(){
+    this.status1 = !this.status1;
+    this.status = false;
+  }
+  clickEvent(){
+    this.status = !this.status;
+  }
+  ngAfterViewInit() {
+    $(".select2").select2({ placeholder: "Search..." });
+  }
+  openVerticallyCentered(content: any) {
+    this.modalService.open(content, { centered: true });
+  }
 }
