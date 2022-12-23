8020d8bdf11a73a2af0fc1ee9b22076df6fcc363
roshaan9035 <shanijutt9035@gmail.com>
2022-12-23 13:26:14 +0500

    Auto Accessories classified form wip

--- src/app/classified-module/motors/auto-accessories-parts/auto-accessories-parts.component.ts
+++ src/app/classified-module/motors/auto-accessories-parts/auto-accessories-parts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Select2 } from 'select2';
+import { AppService } from 'src/app/service/app.service';
 
 @Component({
   selector: 'app-auto-accessories-parts',
@@ -7,12 +8,28 @@ import { Select2 } from 'select2';
   styleUrls: ['./auto-accessories-parts.component.scss']
 })
 export class AutoAccessoriesPartsComponent implements OnInit {
-  ads= 'assets/images/post-add.jpg'
-  constructor() { }
+  ads = 'assets/images/post-add.jpg'
+  AccessoriesUsage: any = [];
+  SellerType : any = [];
+  ToolCondition : any = [];
+  constructor(private service: AppService) {
+    this.service.GetClassifiedLookUpsByCategory("AccessoriesUsage").subscribe((result:any) => {
+      this.AccessoriesUsage = result.data;
+      console.log(this.AccessoriesUsage)
+    })
+    this.service.GetClassifiedLookUpsByCategory("ToolCondition").subscribe((result:any) => {
+      this.ToolCondition = result.data;
+      console.log(this.ToolCondition)
+    })
+    this.service.GetClassifiedLookUpsByCategory("SellerType ").subscribe((result:any) => {
+      this.SellerType  = result.data;
+      console.log(this.SellerType )
+    })
+   }
 
   ngOnInit(): void {
   }
   ngAfterViewInit(): void {
     $('.select2').select2({ placeholder: "Search..." });
-    }
+  }
 }
