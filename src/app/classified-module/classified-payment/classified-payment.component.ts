bea9033589a1b8835b44e03de27604378c336926
pankaj sharma <pankajdxba@hotmail.com>
2022-12-24 13:40:44 +0530

    Design Changes For Classified Payment

---
+++ src/app/classified-module/classified-payment/classified-payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-classified-payment',
+  templateUrl: './classified-payment.component.html',
+  styleUrls: ['./classified-payment.component.scss']
+})
+export class ClassifiedPaymentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
