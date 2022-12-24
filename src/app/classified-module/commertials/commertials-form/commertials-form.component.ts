0d8c7d6aa28c9022bb17587526a29aabb8eda6f3
luqman.webs <luqman.webs@gmail.com>
2022-12-24 20:25:09 +0500

    commercial classified wip

--- src/app/classified-module/commertials/commertials-form/commertials-form.component.ts
+++ src/app/classified-module/commertials/commertials-form/commertials-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Select2 } from 'select2';
-
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-commertials-form',
   templateUrl: './commertials-form.component.html',
@@ -8,12 +9,62 @@ import { Select2 } from 'select2';
 })
 export class CommertialsFormComponent implements OnInit {
   ads= 'assets/images/post-add.jpg'
-  constructor() { }
+  error: any = ""
+  showError: boolean = false;
+  showLoader: boolean = false;
+  classifiedData: any = localStorage.getItem("classifiedData");
+  errorResponse(data: any) {
+    this.showError = false;
+    this.animate();
+  }
+  animate() {
+    let temp: any = $("." + this.currentField).offset()?.top;
+    $("." + this.currentField).addClass("blink");
+    $("." + this.currentField).on("click", () => {
+      $("." + this.currentField).removeClass("blink");
+    })
+    $(window).scrollTop(temp - 100);
+  }
+  currentField: any;
+  DetailsForm = new FormGroup({
+    title: new FormControl("", Validators.required),
+    Phone: new FormControl("", Validators.required),
+    Description: new FormControl("", Validators.required),
+  });
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-  ngAfterViewInit(): void {
-    $('.select2').select2({ placeholder: "Search..." });
-    }
+ngAfterViewInit(): void {
+    $('.select2').select2();
+    $('input[type="number"]').on('input', (e) => {
+      let temp: any = $(e.currentTarget).val();
+      temp.replace(/[^0-9 +]+/, '');
+      $(e.currentTarget).val(temp);
+    });
+  }
+  onSubmit() {
+    if (this.DetailsForm.value.title == "") {
+      this.currentField = "title-input";
+      this.error = "Enter Title";
+      this.showError = true;
+      return;
+    } else if (this.DetailsForm.value.Phone == "") {
+      this.currentField = "phone-input";
+      this.error = "Enter Phone Number";
+      this.showError = true;
+      return;
+    } else if ($(".Show-Phone-Number").val() == 0) {
+      this.currentField = "Show-Phone-Number + .select2";
+      this.error = "Do you want to Show Phone Number";
+      this.showError = true;
+      return;
+    } else if (this.DetailsForm.value.Description == "") {
+      this.currentField = "description-input";
+      this.error = "Enter Price";
+      this.showError = true;
+      return;
+    } 
+  }
 }
 
