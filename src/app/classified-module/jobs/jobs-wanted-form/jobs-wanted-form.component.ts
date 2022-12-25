d50b547a90272a7523a7a459d1ec5d3962a28641
luqman.webs <luqman.dxb@hotmail.com>
2022-12-25 06:58:09 +0500

    job wanted form done

--- src/app/classified-module/jobs/jobs-wanted-form/jobs-wanted-form.component.ts
+++ src/app/classified-module/jobs/jobs-wanted-form/jobs-wanted-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Select2 } from 'select2';
 import { AppService } from 'src/app/service/app.service';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-jobs-wanted-form',
@@ -12,6 +13,8 @@ export class JobsWantedFormComponent implements OnInit {
   ads= 'assets/images/post-add.jpg'
   error: any = ""
   showError: boolean = false;
+  showLoader: boolean = false;
+  classifiedData: any = localStorage.getItem("classifiedData");
   errorResponse(data: any) {
     this.showError = false;
     this.animate();
@@ -34,6 +37,7 @@ export class JobsWantedFormComponent implements OnInit {
   Career : any= [];
   MonthlySalary : any= [];
   WorkExperience : any= [];
+  cv:any = "";
   DetailsForm = new FormGroup({
     title: new FormControl("", Validators.required),
     Phone: new FormControl("", Validators.required),
@@ -41,6 +45,7 @@ export class JobsWantedFormComponent implements OnInit {
     Company: new FormControl("", Validators.required),
   });
   constructor(private service : AppService, private router : Router) {
+    this.classifiedData = JSON.parse(this.classifiedData);
     this.service.GetClassifiedLookUpsByCategory("Gender").subscribe((result:any) => {
       this.Gender = result.data;
     })
@@ -80,6 +85,9 @@ export class JobsWantedFormComponent implements OnInit {
   }
    handleChange(a: any) {
       this.file = a.target.files[0].name;
+      let extension: any = a.target.files[0].name.split(".");
+      extension = extension[extension.length - 1];
+      this.cv = { "FileName": a.target.files[0].name, "Extension": extension, file: a.target.files[0] };
   }
   onSubmit() {
     if (this.DetailsForm.value.title == "") {
@@ -122,37 +130,80 @@ export class JobsWantedFormComponent implements OnInit {
       this.error = "Enter Notice Period";
       this.showError = true;
       return;
-    } else if ($(".visa-status-inputs").val() == 0) {
-      this.currentField = "visa-status-inputs+ .select2";
-      this.error = "Enter Visa Status";
-      this.showError = true;
-      return;
-    } else if ($(".career-level-input").val() == 0) {
-      this.currentField = "career-level-input + .select2";
-      this.error = "Enter Career Level";
-      this.showError = true;
-      return;
     } else if ($(".work-experience-input").val() == 0) {
       this.currentField = "work-experience-input + .select2";
       this.error = "Enter Work Exerience";
       this.showError = true;
       return;
-    } else if ($(".education-level-input").val() == 0) {
-      this.currentField = "education-level-input + .select2";
-      this.error = "Enter Educaton Level";
-      this.showError = true;
-      return;
-    } else if ($(".monthly-salary-input").val() == 0) {
-      this.currentField = "monthly-salary-input + .select2";
-      this.error = "Enter Monthly Salary";
-      this.showError = true;
-      return;
-    } else if ($(".commitment-input").val() == 0) {
-      this.currentField = "commitment-input + .select2";
-      this.error = "Enter Commitment";
+    } else if (this.cv == 0) {
+      this.currentField = "upload-file-text-btn";
+      this.error = "Upload CV";
       this.showError = true;
       return;
     }
+    let tempData: any = {};
+    tempData.CountryId = this.classifiedData.countryId;
+    tempData.CityId = this.classifiedData.cityId;
+    tempData.DistrictId = this.classifiedData.districtId;
+    tempData.Latitude = this.classifiedData.Latitude;
+    tempData.Longitude = this.classifiedData.Longitude;
+    tempData.ClassifiedCategoryId = this.classifiedData.classifiedData[this.classifiedData.classifiedData.length - 1].value;
+    tempData.Title = this.DetailsForm.value.title;
+    tempData.Description = this.DetailsForm.value.Description;
+    tempData.PhoneNumber = this.DetailsForm.value.Phone;
+    tempData.GenderId = $(".Gender").val();
+    tempData.NationalityId = $(".Nationality").val();
+    tempData.CurrentLocationId = $(".Current-location").val();
+    tempData.CurrentLocationId = $(".Current-location").val();
+    tempData.CompanyName = this.DetailsForm.value.Company;
+    tempData.NoticePeriodId = $(".notice-period-input").val();
+    tempData.CareerLevelId = $(".career-level-input").val();
+    tempData.WorkExperienceId = $(".work-experience-input").val();
+    tempData.Documents = [{"FileId":1,"IsMainImage":1,"FileName":this.cv.FileName,"Extension":this.cv.Extension}];
+
+    let userData:any = localStorage.getItem("user");
+    userData = JSON.parse(userData)
+    tempData.UserId = userData.id;
+    console.log(tempData);
+    localStorage.setItem("classifiedFormData",JSON.stringify(tempData));
+    let formData = new FormData();
+    formData.append("ClassifiedRequest",JSON.stringify(tempData));
+    formData.append("1_" + this.cv.FileName,this.cv.file);
+    let token: any = localStorage.getItem("token");
+    token = JSON.parse(token);
+    this.showLoader = true;
+    $.ajax({
+      url: `${environment.apiUrl}api/AddUpdateClassified`,
+      method: "post",
+      contentType: false,
+      processData: false,
+      data: formData,
+      headers: {
+        "Authorization": 'bearer ' + token
+      },
+      dataType: "json",
+      success: (res) => {
+        console.log(res);
+        if (res.result == 1) {
+          this.showLoader = false;
+          tempData.id = res.data.id;
+          localStorage.setItem("classifiedFormData", JSON.stringify(tempData));
+          if(this.classifiedData.classifiedData[0].value == 1) {
+            this.router.navigate(["/classified/classified-payment"]);
+          } else {
+            this.router.navigate(["/classified/classified-payment-second"]);
+          }
+        }
+        else {
+          this.showLoader = false;
+          this.error = res.error;
+          this.showError = true;
+        }
+      },
+      error: (err) => {
+        this.showLoader = false;
+      }
+    });
   }
 }
 
