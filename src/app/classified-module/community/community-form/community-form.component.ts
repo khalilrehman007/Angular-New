de84dea09f737f87b47afe2508fee1de5eb536b7
luqman.webs <luqman.dxb@hotmail.com>
2022-12-26 03:15:57 +0500

    community + commertial form done

--- src/app/classified-module/community/community-form/community-form.component.ts
+++ src/app/classified-module/community/community-form/community-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Select2 } from 'select2';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-community-form',
@@ -26,13 +27,21 @@ export class CommunityFormComponent implements OnInit {
     $(window).scrollTop(temp - 100);
   }
   currentField: any;
+  selectedFiles: any;
+  imageData: any = [];
+  previews: string[] = [];
+  mainImage: any = 0;
+  data: any = {};
+  imgCheck: boolean = false;
 
   DetailsForm = new FormGroup({
     title: new FormControl("", Validators.required),
     Phone: new FormControl("", Validators.required),
     Description: new FormControl("", Validators.required),
   });
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    this.classifiedData = JSON.parse(this.classifiedData);
+  }
 
   ngOnInit(): void {
   }
@@ -65,7 +74,125 @@ export class CommunityFormComponent implements OnInit {
       this.error = "Enter Description";
       this.showError = true;
       return;
-    } 
+    }
+    let tempData:any = {};
+    tempData.CountryId = this.classifiedData.countryId;
+    tempData.CityId = this.classifiedData.cityId;
+    tempData.DistrictId = this.classifiedData.districtId;
+    tempData.Latitude = this.classifiedData.Latitude;
+    tempData.Longitude = this.classifiedData.Longitude;
+    tempData.ClassifiedCategoryId = this.classifiedData.classifiedData[this.classifiedData.classifiedData.length - 1].value;
+    tempData.Title = this.DetailsForm.value.title;
+    tempData.Description = this.DetailsForm.value.Description;
+    tempData.PhoneNumber = this.DetailsForm.value.Phone;
+    
+    let tempImg:any = [];
+    for (let i = 0; i < this.imageData.length; i++) {
+      let temp:any = {};
+      temp.FileId = i+1;
+      temp.FileName = this.imageData[i].FileName;
+      temp.Extension = this.imageData[i].Extension;
+      temp.IsMainImage = 0;
+      if(this.mainImage == i) {
+        temp.IsMainImage = 1;
+      }
+      tempImg.push(temp);
+    }
+    tempData.Documents = tempImg;
+
+    let userData:any = localStorage.getItem("user");
+    userData = JSON.parse(userData)
+    tempData.UserId = userData.id;
+    console.log(tempData);
+    localStorage.setItem("classifiedFormData",JSON.stringify(tempData));
+
+    let formData = new FormData();
+    formData.append("ClassifiedRequest",JSON.stringify(tempData));
+    for (let i = 0; i < this.imageData.length; i++) {
+      formData.append(i + 1 + "_" + this.imageData[i].FileName, this.imageData[i].file);
+    }
+    let token: any = localStorage.getItem("token");
+    token = JSON.parse(token);
+    this.showLoader = true;
+    $.ajax({
+      url: `${environment.apiUrl}api/AddUpdateClassified`,
+      method: "post",
+      contentType: false,
+      processData: false,
+      data: formData,
+      headers: {
+        "Authorization": 'bearer ' + token
+      },
+      dataType: "json",
+      success: (res) => {
+        console.log(res);
+        if (res.result == 1) {
+          this.showLoader = false;
+          tempData.id = res.data.id;
+          localStorage.setItem("classifiedFormData", JSON.stringify(tempData));
+          if(this.classifiedData.classifiedData[0].value == 1) {
+            this.router.navigate(["/classified/classified-payment"]);
+          } else {
+            this.router.navigate(["/classified/classified-payment-second"]);
+          }
+        }
+        else {
+          this.showLoader = false;
+          this.error = res.error;
+          this.showError = true;
+        }
+      },
+      error: (err) => {
+        this.showLoader = false;
+      }
+    });
+  }
+  selectFiles(event: any): void {
+    let check = true;
+    let temp: number = 0;
+    for (let i = 0; i < event.target.files.length; i++) {
+      if (event.target.files[i].size / 1048576 > 2) {
+        this.error = "Maximun size allowed is 2MB per Image";
+        this.showError = true;
+        check = false;
+        return;
+      }
+      // temp += event.target.files[i].size
+    }
+    if (check) {
+      temp = temp / 1048576
+      if (temp > 10) {
+        alert("Maximun size allowed is 10MB");
+        return;
+      }
+      // this.message = [];
+      // this.progressInfos = [];
+      this.selectedFiles = event.target.files;
+      for (let i = 0; i < this.selectedFiles.length; i++) {
+        let extension: any = this.selectedFiles[i].name.split(".");
+        extension = extension[extension.length - 1];
+        this.imageData.push({ "FileName": this.selectedFiles[i].name, "Extension": extension, file: this.selectedFiles[i] });
+      }
+      if (this.selectedFiles && this.selectedFiles[0]) {
+        const numberOfFiles = this.selectedFiles.length;
+        for (let i = 0; i < numberOfFiles; i++) {
+          const reader = new FileReader();
+          reader.onload = (e: any) => {
+            this.previews.push(e.target.result);
+          };
+          reader.readAsDataURL(this.selectedFiles[i]);
+        }
+      }
+      $(".image-input").val("");
+    }
+  }
+  removeImage(index: any) {
+    this.mainImage = 0;
+    this.previews.splice(index, 1);
+    this.data?.Documents.splice(index, 1);
+  }
+  changeMainImg(index: any) {
+    this.mainImage = index;
   }
 }
 
